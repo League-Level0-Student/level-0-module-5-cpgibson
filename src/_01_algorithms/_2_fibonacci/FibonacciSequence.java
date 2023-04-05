2f50163d285476f9533ca5197557d15677fdc12c
root <root@4577b4575dfa>
2023-04-05 01:23:02 +0000

    sg

--- src/_01_algorithms/_2_fibonacci/FibonacciSequence.java
+++ src/_01_algorithms/_2_fibonacci/FibonacciSequence.java
@@ -2,4 +2,17 @@ package _01_algorithms._2_fibonacci;
 
 public class FibonacciSequence {
 
+	public static void main(String[] args) {
+		int number1= 0;
+		int number2= 1;
+		System.out.println(number1);
+		System.out.println(number2);
+		for (int i = 1; i <= 10; i++){
+			int sum = (number1 + number2);
+			number1 = number2;
+			number2 = sum;
+			System.out.println(sum);
+		}
+	}
+	
 }
