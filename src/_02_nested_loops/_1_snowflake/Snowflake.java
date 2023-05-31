ea98f4ffc74133d2fad2e0ea67af732622d6f0c0
root <root@31a16e16e410>
2023-05-31 01:30:14 +0000

    cdaca

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,25 +2,32 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-System.out.println("hello");
-
+Robot rob = new Robot ();
 		// #2. Set your robot’s position to x=300 and y=300
-
+rob.moveTo(300, 300);
 
 		// #3. Put the robot's pen down
-
+rob.penDown();
 
 		// #4. Set the robot’s speed to 5
 
-
+rob.setSpeed(5);
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
+for (int i=0; i<=5; i++) {
+	//(255, 0, 0)
+	// Red, Green, Blue
+	// (Color.BLUE)
+	rob.setPenColor(Color.BLACK);
+	
+}
 			
 			// #6. Set the pen color to black
 
